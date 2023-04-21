71aca609d8c8e06d9c916453b14f6a938088dda1
sumathanvanthan <novac.arun@gmail.com>
2023-04-21 21:27:21 +0530

    initial

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,17 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-M3E4P447GJ"></script>
+<script dangerouslySetInnerHTML={{
+    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','G-M3E4P447GJ');`,
+  }}>
+  </script>
+      </Head>      
       <body>
         <Main />
         <NextScript />
